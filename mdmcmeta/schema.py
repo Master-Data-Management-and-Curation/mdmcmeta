a9cd4fe50d7cc18b5be684c49a400b21ebefe501
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2024-10-03 12:59:43 +0200

    implement schema for metadata exchange
    both get and create models

---
+++ mdmcmeta/schema.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class MetadataCreate(BaseModel):
+    filename: str
+    username: str
+    width: int
+    height: int
+    n_channels: int
+    date: datetime
+    size: int
+
+
+class MetadataGet(MetadataCreate):
+    id: int
