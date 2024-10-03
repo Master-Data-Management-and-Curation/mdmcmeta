13b7bcde86b95e7c907cbec94518c773186ec1c9
mattiamare9 <mattia.marella@edu.unife.it>
2024-10-03 17:19:24 +0200

    corrected bug in the id generation

--- mdmcmeta/database.py
+++ mdmcmeta/database.py
@@ -1,10 +1,14 @@
-import MetadataCreate
+from schema import MetadataCreate
 
 class Database:
     __data : dict = {}
+    __id_generator : int
+
+    def __init__(self):
+        self.__id_generator = self.__gen_id()
 
     def store(self, obj:MetadataCreate):
-        id = next(self.__gen_id())
+        id = next(self.__id_generator)
         self.__data[id] = obj
     
     def __gen_id(self):
