118b9dbf86668bd36d5d7b5045e4ef94a6e19675
NicolaPerin <nicolaperin1998@gmail.com>
2024-10-03 13:00:09 +0200

    implement schema for metadata exchange

---
+++ mdmcmeta/schema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class MetadataCreate(BaseModel):
+    filename: str
+    username: str
+    width: int
+    height: int
+    n_channels: int
+    date: datetime
+    size: int
+
+class MetadataGet(MetadataCreate):
+    id: int
