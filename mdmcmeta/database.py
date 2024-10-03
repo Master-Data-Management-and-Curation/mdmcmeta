054547dfa2289ae624e9bc24faf1f75bf36d0168
mattiamare9 <mattia.marella@edu.unife.it>
2024-10-03 16:54:01 +0200

    Database class: added store method, added index generator

--- mdmcmeta/database.py
+++ mdmcmeta/database.py
@@ -1,8 +1,14 @@
-from schema import MetadataCreate
+import MetadataCreate
 
 class Database:
-	__data : dict = {}
-	__last_id : int
+    __data : dict = {}
 
-	def store(self, obj):
-		raise NotImplementedError
+    def store(self, obj:MetadataCreate):
+        id = next(self.__gen_id())
+        self.__data[id] = obj
+    
+    def __gen_id(self):
+        num = 0
+        while True:
+            yield num
+            num += 1
