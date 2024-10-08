085f57b5f433ae825e713f0f6e2df92161c84f00
LucaPuricelli <luca.puricelli85@gmail.com>
2024-10-08 16:04:32 +0200

    Modified img_importer.py

--- mdmcmeta/client/img_importer.py
+++ mdmcmeta/client/img_importer.py
@@ -5,34 +5,23 @@ from PIL import Image
 from PIL import UnidentifiedImageError
 import os
 
-def read_images(input_dir):
-    try:
-        files = input_dir.glob("*.png")
-        if files == [] :
-            print ('Non sono stati trovati file di tipo png')
-        else:
-            return list(files)
-    except UnidentifiedImageError as e:
-        print(f'Errore: {e}',file=sys.stderr)
-        return
-    except OSError as e:
-        print('Errore di apertura file: {e}', file=sys.stderr)
-    return
-
 def read_image(input_file):
     ext=input_file.suffix
     if ext != '.png' :
-            print ('Il file non è di tipo png')
-            return
+            raise Exception('Il file non è di tipo png')
+            #print ('Il file non è di tipo png')
+            #return
     else:
         try:
             image=Image.open(input_file)
         except UnidentifiedImageError as e:
-            print(f'Errore: {e}',file=sys.stderr)
-            return
+            raise e
+            #print(f'Errore: {e}',file=sys.stderr)
+            #return
         except OSError as e:
-            print('Errore di apertura file: {e}', file=sys.stderr)
-            return
+            raise e
+            #print('Errore di apertura file: {e}', file=sys.stderr)
+            #return
         return image
 
 if __name__ == "__main__":
